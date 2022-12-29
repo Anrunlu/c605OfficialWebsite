a1bb9be2d231dca49762c857fc903a7c7947cf7a
安润鲁 <anrunlu@163.com>
2022-12-29 21:02:04 +0800

    opt

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,11 +1,59 @@
 <template>
-  <q-page class="flex flex-center">
-    <h1>首页</h1>
+  <q-page>
+    <div class="row justify-center q-mt-md">
+      <div class="col col-md-8">
+        <q-carousel
+          v-model="slide"
+          transition-prev="scale"
+          transition-next="scale"
+          swipeable
+          animated
+          control-color="white"
+          navigation
+          padding
+          arrows
+          height="300px"
+          class="bg-primary text-white shadow-1 rounded-borders"
+        >
+          <q-carousel-slide name="style" class="column no-wrap flex-center">
+            <q-icon name="style" size="56px" />
+            <div class="q-mt-md text-center">
+              {{ lorem }}
+            </div>
+          </q-carousel-slide>
+          <q-carousel-slide name="tv" class="column no-wrap flex-center">
+            <q-icon name="live_tv" size="56px" />
+            <div class="q-mt-md text-center">
+              {{ lorem }}
+            </div>
+          </q-carousel-slide>
+          <q-carousel-slide name="layers" class="column no-wrap flex-center">
+            <q-icon name="layers" size="56px" />
+            <div class="q-mt-md text-center">
+              {{ lorem }}
+            </div>
+          </q-carousel-slide>
+          <q-carousel-slide name="map" class="column no-wrap flex-center">
+            <q-icon name="terrain" size="56px" />
+            <div class="q-mt-md text-center">
+              {{ lorem }}
+            </div>
+          </q-carousel-slide>
+        </q-carousel>
+      </div>
+    </div>
   </q-page>
 </template>
 
 <script>
 export default {
   name: "Index",
+  data() {
+    return {
+      lorem:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut imperdiet nisi. Sed euismod, nisl vel ultricies aliquam, nunc nisl tincidunt nunc, eget aliquam nunc nisl eget nunc. Sed euismod, nisl vel ultricies aliquam, nunc nisl tincidunt nunc, eget aliquam nunc nisl eget nunc.",
+      slide: "style",
+    };
+  },
 };
 </script>
--- src/pages/Projects.vue
+++ src/pages/Projects.vue
@@ -17,7 +17,10 @@
             :label="project.name"
             v-for="(project, index) in projectList"
             :key="index"
-          />
+            :alert="project.alert ? project.alertColor : false"
+            :alert-icon="project.alertIcon"
+          >
+          </q-tab>
         </q-tabs>
       </div>
     </div>
@@ -56,12 +59,37 @@
             <template v-slot:title>
               <!-- 事件标题 -->
               <div class="text-h6">{{ event.title }}</div>
-              <!-- 主要参与成员 -->
+              <!-- 参与成员，目前只分两级，主要的和次要的 -->
               <div v-if="event.members.length > 0" class="q-mt-xs q-gutter-xs">
                 <q-chip
                   square
                   :dense="$q.screen.lt.sm"
-                  v-for="(member, index) in event.members"
+                  v-for="(member, index) in event.members.filter(
+                    (member) => member.role == 'primary'
+                  )"
+                  :key="index"
+                  class="cursor-pointer"
+                  size="sm"
+                  outline
+                >
+                  <q-avatar>
+                    <img :src="member.avatar" />
+                  </q-avatar>
+                  {{ member.name }}
+
+                  <q-tooltip>
+                    {{ member.position }}
+                  </q-tooltip>
+                </q-chip>
+
+                <q-space />
+
+                <q-chip
+                  square
+                  :dense="$q.screen.lt.sm"
+                  v-for="(member, index) in event.members.filter(
+                    (member) => member.role == 'secondary'
+                  )"
                   :key="index"
                   class="cursor-pointer"
                   size="sm"
@@ -108,7 +136,11 @@ const projectList = [
     _id: 0,
     name: "知新",
     icon: "img:https://zx-stu.anrunlu.net/favicon.ico",
+    role: "primary",
     themeColor: "primary",
+    alert: true,
+    alertColor: "red",
+    alertIcon: "military_tech",
   },
   {
     _id: 1,
@@ -119,7 +151,7 @@ const projectList = [
   {
     _id: 2,
     name: "疫先锋",
-    icon: "apps",
+    icon: "img:https://cyberdownload.anrunlu.net/zhixin2.1shot/yixianf2.png",
     themeColor: "accent",
   },
   {
@@ -127,6 +159,12 @@ const projectList = [
     name: "学科竞赛",
     icon: "img:http://www.52jingsai.com/favicon.ico",
     themeColor: "orange",
+  },
+  {
+    _id: 4,
+    name: "其他项目",
+    icon: "apps",
+    themeColor: "primary",
   },
 ];
 
@@ -158,6 +196,7 @@ const projectDetailsList = [
             name: "安润鲁",
             avatar: "https://cyberdownload.anrunlu.net/zhixin2.1shot/arl6.png",
             position: "平台架构负责人、平台全栈开发",
+            role: "primary",
             description: "",
           },
         ],
@@ -219,6 +258,7 @@ const projectDetailsList = [
             avatar:
               "https://cyberdownload.anrunlu.net/2021412977-1671455059780.png",
             position: "平台架构负责人、平台全栈开发",
+            role: "primary",
             description: "",
           },
         ],
@@ -250,6 +290,7 @@ const projectDetailsList = [
             avatar:
               "https://cyberdownload.anrunlu.net/2021412977-1671455059780.png",
             position: "负责人",
+            role: "primary",
             description: "",
           },
         ],
@@ -286,83 +327,85 @@ const projectDetailsList = [
         content:
           "以“疫先锋——知识与数据双驱动的疫情防控与决策平台”作为此次的参赛项目。该平台实现了从疫情预警、流调分析到预案生成全过程数智化、自动化的疫情防控与决策功能。在项目提交之前，团队成员做了明确的分工，以保证高效率、高质量的完成比赛所需的提交材料。由于疫情政策的改变，团队后期对该平台做了一定的升级，以此来更好的适应当下的政策，同时更好的满足用户需求。",
         imgs: [
-          "https://cyberdownload.anrunlu.net/zhixin2.1shot/shqy1.jpg",
-          "https://cyberdownload.anrunlu.net/zhixin2.1shot/shqy2.jpg",
-          "https://cyberdownload.anrunlu.net/zhixin2.1shot/shqy3.jpg",
-          "https://cyberdownload.anrunlu.net/zhixin2.1shot/shqy4.jpg",
+          "http://cyberdownload.anrunlu.net/2021412977-1671454863369.png",
+          "http://cyberdownload.anrunlu.net/2021412977-1671454846869.png",
+          "http://cyberdownload.anrunlu.net/2021412977-1671454520923.png",
+          "http://cyberdownload.anrunlu.net/2021412977-1671454388285.png",
+          "http://cyberdownload.anrunlu.net/2021412984-1672314103654.png",
+          "http://cyberdownload.anrunlu.net/2021412984-1672314097225.png",
         ],
         members: [
           {
             name: "王茂励",
             avatar:
-              "https://cyberdownload.anrunlu.net/2021412977-1671455059780.png",
+              "http://cyberdownload.anrunlu.net/2021412977-1671455043230.png",
             position: "指导老师",
-            role: "teacher",
+            role: "secondary",
             description: "网络空间安全学院院长",
           },
           {
             name: "周子力",
             avatar:
-              "https://cyberdownload.anrunlu.net/2021412977-1671455059780.png",
+              "http://cyberdownload.anrunlu.net/2021412977-1671455059780.png",
             position: "指导老师",
-            role: "teacher",
+            role: "secondary",
             description: "副教授",
           },
           {
             name: "范瑞红",
             avatar:
-              "https://cyberdownload.anrunlu.net/2021412977-1671455059780.png",
+              "http://cyberdownload.anrunlu.net/2021412977-1671455081085.png",
             position: "指导老师",
-            role: "teacher",
+            role: "secondary",
             description: "网络空间安全学院书记",
           },
           {
             name: "徐沛玉",
             avatar:
-              "https://cyberdownload.anrunlu.net/2021412977-1671455059780.png",
+              "http://cyberdownload.anrunlu.net/2021412977-1671455126431.jpg",
             position: "项目负责人",
-            role: "student",
+            role: "primary",
             description: "物联网工程",
           },
           {
             name: "李林",
             avatar:
-              "https://cyberdownload.anrunlu.net/2021412977-1671455059780.png",
+              "http://cyberdownload.anrunlu.net/2021412977-1671455153304.jpg",
             position: "团队成员",
-            role: "student",
+            role: "primary",
             description: "软件工程",
           },
           {
             name: "安润鲁",
             avatar:
-              "https://cyberdownload.anrunlu.net/2021412977-1671455059780.png",
+              "http://cyberdownload.anrunlu.net/2021412977-1671455099712.jpg",
             position: "团队成员",
-            role: "student",
+            role: "primary",
             description: "智能数据",
           },
           {
             name: "张珠琛",
             avatar:
-              "https://cyberdownload.anrunlu.net/2021412977-1671455059780.png",
+              "http://cyberdownload.anrunlu.net/2021412977-1671455184591.jpg",
             position: "团队成员",
-            role: "student",
-            description: "软件开发",
+            role: "primary",
+            description: "软件工程",
           },
           {
             name: "田丰",
             avatar:
-              "https://cyberdownload.anrunlu.net/2021412977-1671455059780.png",
+              "http://cyberdownload.anrunlu.net/2021412977-1671455199522.jpg",
             position: "团队成员",
-            role: "student",
-            description: "软件开发",
+            role: "primary",
+            description: "软件工程",
           },
           {
             name: "王寒寒",
             avatar:
-              "https://cyberdownload.anrunlu.net/2021412977-1671455059780.png",
+              "http://cyberdownload.anrunlu.net/2021412977-1671455214709.jpg",
             position: "团队成员",
-            role: "student",
-            description: "软件开发",
+            role: "primary",
+            description: "软件工程",
           },
         ],
       },
@@ -374,74 +417,84 @@ const projectDetailsList = [
         content:
           "此次比赛，一共有三个参赛作品，分别是——曲际岛--基于认知智能与智慧网络的大学社交元宇宙、疫先锋——知识与数据双驱动的疫情防控与决策平台、知新——个性化教学过程管理平台，这三个参赛作品分别由三位负责人带领。在这三个项目中，曲际岛项目的整改工作相对较多，所以比赛前的准备工作主要放在了该项目，时间紧任务重，不过由于团队成员合理的时间分配，顺利完成了比赛准备工作。",
         imgs: [
-          "https://cyberdownload.anrunlu.net/zhixin2.1shot/shqy1.jpg",
-          "https://cyberdownload.anrunlu.net/zhixin2.1shot/shqy2.jpg",
-          "https://cyberdownload.anrunlu.net/zhixin2.1shot/shqy3.jpg",
-          "https://cyberdownload.anrunlu.net/zhixin2.1shot/shqy4.jpg",
+          "http://cyberdownload.anrunlu.net/2021412984-1672315115823.jpg",
+          "http://cyberdownload.anrunlu.net/2021412984-1672315108197.jpg",
+          "http://cyberdownload.anrunlu.net/2021412984-1672315102799.jpg",
+          "http://cyberdownload.anrunlu.net/2021412984-1672315089842.png",
+          "http://cyberdownload.anrunlu.net/2021412984-1672315070704.png",
+          "http://cyberdownload.anrunlu.net/2021412977-1671454933467.png",
         ],
         members: [
           {
             name: "王茂励",
             avatar:
-              "https://cyberdownload.anrunlu.net/2021412977-1671455059780.png",
+              "http://cyberdownload.anrunlu.net/2021412977-1671455043230.png",
             position: "指导老师",
-            role: "teacher",
+            role: "secondary",
             description: "网络空间安全学院院长",
           },
           {
             name: "周子力",
             avatar:
-              "https://cyberdownload.anrunlu.net/2021412977-1671455059780.png",
+              "http://cyberdownload.anrunlu.net/2021412977-1671455059780.png",
             position: "指导老师",
-            role: "teacher",
+            role: "secondary",
             description: "副教授",
           },
           {
             name: "范瑞红",
             avatar:
-              "https://cyberdownload.anrunlu.net/2021412977-1671455059780.png",
+              "http://cyberdownload.anrunlu.net/2021412977-1671455081085.png",
             position: "指导老师",
-            role: "teacher",
+            role: "secondary",
             description: "网络空间安全学院书记",
           },
           {
             name: "徐沛玉",
             avatar:
-              "https://cyberdownload.anrunlu.net/2021412977-1671455059780.png",
+              "http://cyberdownload.anrunlu.net/2021412977-1671455126431.jpg",
             position: "项目负责人",
-            role: "student",
+            role: "primary",
             description: "物联网工程",
           },
           {
             name: "李林",
             avatar:
-              "https://cyberdownload.anrunlu.net/2021412977-1671455059780.png",
+              "http://cyberdownload.anrunlu.net/2021412977-1671455153304.jpg",
             position: "团队成员",
-            role: "student",
+            role: "primary",
             description: "软件工程",
           },
           {
             name: "安润鲁",
             avatar:
-              "https://cyberdownload.anrunlu.net/2021412977-1671455059780.png",
+              "http://cyberdownload.anrunlu.net/2021412977-1671455099712.jpg",
             position: "团队成员",
-            role: "student",
+            role: "primary",
             description: "智能数据",
+          },
+          {
+            name: "张珠琛",
+            avatar:
+              "http://cyberdownload.anrunlu.net/2021412977-1671455184591.jpg",
+            position: "团队成员",
+            role: "primary",
+            description: "软件工程",
           },
           {
             name: "田丰",
             avatar:
-              "https://cyberdownload.anrunlu.net/2021412977-1671455059780.png",
+              "http://cyberdownload.anrunlu.net/2021412977-1671455199522.jpg",
             position: "团队成员",
-            role: "student",
+            role: "primary",
             description: "软件工程",
           },
           {
             name: "王寒寒",
             avatar:
-              "https://cyberdownload.anrunlu.net/2021412977-1671455059780.png",
+              "http://cyberdownload.anrunlu.net/2021412977-1671455214709.jpg",
             position: "团队成员",
-            role: "student",
+            role: "primary",
             description: "软件工程",
           },
         ],
@@ -454,77 +507,94 @@ const projectDetailsList = [
         content:
           "此次比赛以“明冠沁心”——数字大脑平台作为比赛的参赛项目，该平台的设计意在体现当下个性化教学、因材施教的教学理念；在报名提交材料之前，团队成员做了具体的分工，工作主要分为两大部分——平台完善、参赛材料的编写。由于团队成员合理的合作，保证了参赛作品的高质量的完成。不负期待，该参赛作品取得了不错的成绩。经过这次比赛，参赛成员懂得了如何合作、如何独立解决问题、如何将学到的知识合理利用。",
         imgs: [
-          "https://cyberdownload.anrunlu.net/zhixin2.1shot/shqy1.jpg",
-          "https://cyberdownload.anrunlu.net/zhixin2.1shot/shqy2.jpg",
-          "https://cyberdownload.anrunlu.net/zhixin2.1shot/shqy3.jpg",
-          "https://cyberdownload.anrunlu.net/zhixin2.1shot/shqy4.jpg",
+          "http://cyberdownload.anrunlu.net/2021412984-1672315807110.png",
+          "http://cyberdownload.anrunlu.net/2021412984-1672315803097.png",
+          "http://cyberdownload.anrunlu.net/2021412984-1672315798433.png",
+          "http://cyberdownload.anrunlu.net/2021412984-1672315794696.png",
+          "http://cyberdownload.anrunlu.net/2021412984-1672315791235.png",
+          "http://cyberdownload.anrunlu.net/2021412984-1672315786176.png",
         ],
         members: [
           {
             name: "王茂励",
             avatar:
-              "https://cyberdownload.anrunlu.net/2021412977-1671455059780.png",
+              "http://cyberdownload.anrunlu.net/2021412977-1671455043230.png",
             position: "指导老师",
-            role: "teacher",
+            role: "secondary",
             description: "网络空间安全学院院长",
           },
           {
             name: "周子力",
             avatar:
-              "https://cyberdownload.anrunlu.net/2021412977-1671455059780.png",
+              "http://cyberdownload.anrunlu.net/2021412977-1671455059780.png",
             position: "指导老师",
-            role: "teacher",
+            role: "secondary",
             description: "副教授",
           },
           {
             name: "范瑞红",
             avatar:
-              "https://cyberdownload.anrunlu.net/2021412977-1671455059780.png",
+              "http://cyberdownload.anrunlu.net/2021412977-1671455081085.png",
             position: "指导老师",
-            role: "teacher",
+            role: "secondary",
             description: "网络空间安全学院书记",
           },
           {
             name: "徐沛玉",
             avatar:
-              "https://cyberdownload.anrunlu.net/2021412977-1671455059780.png",
+              "http://cyberdownload.anrunlu.net/2021412977-1671455126431.jpg",
             position: "项目负责人",
-            role: "student",
+            role: "primary",
             description: "物联网工程",
           },
           {
             name: "李林",
             avatar:
-              "https://cyberdownload.anrunlu.net/2021412977-1671455059780.png",
+              "http://cyberdownload.anrunlu.net/2021412977-1671455153304.jpg",
             position: "团队成员",
-            role: "student",
+            role: "primary",
             description: "软件工程",
           },
           {
             name: "张中健",
-            avatar:
-              "https://cyberdownload.anrunlu.net/2021412977-1671455059780.png",
+            avatar: "https://cdn.quasar.dev/img/boy-avatar.png",
             position: "团队成员",
-            role: "student",
+            role: "primary",
             description: "软件工程",
           },
           {
             name: "安润鲁",
             avatar:
-              "https://cyberdownload.anrunlu.net/2021412977-1671455059780.png",
+              "http://cyberdownload.anrunlu.net/2021412977-1671455099712.jpg",
             position: "团队成员",
-            role: "student",
+            role: "primary",
             description: "智能数据",
           },
           {
             name: "张珠琛",
             avatar:
-              "https://cyberdownload.anrunlu.net/2021412977-1671455059780.png",
+              "http://cyberdownload.anrunlu.net/2021412977-1671455184591.jpg",
             position: "团队成员",
-            role: "student",
+            role: "primary",
             description: "软件工程",
           },
         ],
+      },
+    ],
+  },
+  {
+    _id: 4,
+    name: "其他项目",
+    description: "",
+    link: "https://zx-stu.anrunlu.net",
+    timeline: [
+      {
+        title: "Event Title",
+        subtitle: "February 22, 1986",
+        color: "orange",
+        icon: "done_all",
+        content: "",
+        members: [],
       },
     ],
   },
