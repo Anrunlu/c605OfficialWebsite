f375b7e5c5d53b71223c2bc2d29345da41fe81a7
安润鲁 <anrunlu@163.com>
2022-12-31 22:51:14 +0800

    opt

--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -1,6 +1,16 @@
 <template>
-  <q-page class="flex flex-center">
-    <h1>加入我们</h1>
+  <q-page class="q-my-lg">
+    <div class="row justify-center text-center">
+      <div class="col-10">
+        <h5>加入我们</h5>
+        <q-img
+          src="https://cyberdownload.anrunlu.net/zhixin2.1shot/zhaopin.jpg"
+          spinner-color="primary"
+          spinner-size="82px"
+          width="300px"
+        />
+      </div>
+    </div>
   </q-page>
 </template>
 
