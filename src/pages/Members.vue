381fe43d0adfdb3a2313005f5a37ffa105bf4398
安润鲁 <anrunlu@163.com>
2022-12-31 21:48:09 +0800

    opt

--- src/pages/Members.vue
+++ src/pages/Members.vue
@@ -365,7 +365,7 @@ export default {
     return {
       currMembers: currMembers,
       allMembersJoinTimeline: allMembersJoinTimeline,
-      currCategory: "allMembers",
+      currCategory: "currMembers",
       categorys: [
         {
           name: "currMembers",
